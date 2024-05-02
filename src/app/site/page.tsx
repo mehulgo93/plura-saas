e823c1b8e4f85277055df1cdb4d9dd010887b824
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-05-02 07:02:24 -0400

    setting up landing page

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Page
-    </main>
+    <>
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+        {/* grid */}
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
+        <p>Run your agency, in one place</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            Plura
+          </h1>
+        </div>
+      </section>
+    </>
   );
 }
