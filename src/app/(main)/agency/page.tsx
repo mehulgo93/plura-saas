18f29df48f7538069d5ed688a52741ba4ee5a011
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-05-06 17:14:54 -0400

    adding routes and navigation for agency and sub-accounts

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Agency</div>;
+};
+
+export default page;
--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
-      <section className="flex justify-center items-center flex-col gap-4 mt-20">
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
         <h2 className="text-4xl text-center">Choose what fits you right</h2>
         <p className="text-muted-foreground text-center">
           Our straightforward pricing plans are tailored to meet your needs. If
