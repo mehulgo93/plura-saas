e862ea159ad7bfcf15b3c6e0491c8e2b868b3548
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-05-13 18:49:22 -0400

    adding some queries

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,19 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const authUser = await currentUser();
+  if (!authUser) {
+    return redirect("/sign-in");
+  }
+
+  const agencyId = await verifyAndAcceptInvitation();
+  // get users details
+  // route them to agency or subaccounts
+
+  const user = await getAuthUserDetails();
   return <div>Agency</div>;
 };
 
